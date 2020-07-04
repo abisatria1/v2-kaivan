d46a6d7c2e07af6e2ecb339f719903e6358f9c37
Abisatria1 <abi.kadek@gmail.com>
2020-07-05 04:53:02 +0800

    menambahkan order validator

---
+++ helpers/validator/orderValidator.js
@@ -0,0 +1,21 @@
+const {response,customError} = require('../wrapper')
+const Customer = require('../../models/Customer')
+const Driver = require('../../models/Driver')
+const Partner = require('../../models/Partner')
+
+const setCustomer = () => {
+    return async (req,res,next) => {
+        // kurang update contact jika mengubah keterangan
+        const {customer} = req.body
+        if (customer.googleId != "" || customer.googleId != undefined || customer.googleId != null) {
+            // jika google ID tidak ada maka buat contact baru di google contact
+        }
+        const result = await Customer.create(customer)
+        req.customer = result
+        next()
+    }
+}
+
+module.exports = {
+    setCustomer
+}
