361062d4b8bdda3de1e2a342ae27bf7c69e3cd48
Abisatria1 <abi.kadek@gmail.com>
2020-07-07 04:50:17 +0800

    menghilangkan log database

--- config/database.js
+++ config/database.js
@@ -6,7 +6,8 @@ const db = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host : process.env.DB_HOST,
-        dialect : 'mysql'
+        dialect : 'mysql',
+        logging: false
     }
 )
 
