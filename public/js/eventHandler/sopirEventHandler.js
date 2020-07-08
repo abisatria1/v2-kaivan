9da8ffa40f8f6a55c609f83deccb43bb5bb3e142
Abisatria1 <abi.kadek@gmail.com>
2020-07-09 02:19:07 +0800

    menambahkan update create and delete pada view sopir

--- public/js/eventHandler/sopirEventHandler.js
+++ public/js/eventHandler/sopirEventHandler.js
@@ -1,30 +1,137 @@
+
+
 $(document).ready( async () => {
     const sopirTable = defineDataTable()
     await drawTable(sopirTable)
+    const modal = $('#modal').iziModal({
+        title : 'Tambah Data Sopir',
+        subtitle : 'Diharapkan mengisi data dengan benar dan bertanggung jawab',
+        headerColor : '#CF5300',
+        closeButton : false,
+        width : 700,
+        onOpening : () => {
+            $('#createSopir').removeClass('was-validated')
+        }
+    }) 
+
+    // create
+    $(document).on('submit','form',async (e) => {
+        createValidation()
+        e.preventDefault()
+        console.log(e.target)
+        if (e.target.checkValidity() !== false) {
+            // creating
+            try {
+                const form = $('form')
+                $('#createForm .button').hide()
+                $('#createForm .loading').removeClass('d-none')
+                const data = form.serialize()
+                await createSopir(data)
+                drawTable(sopirTable)
+                $('#createForm .button').show()
+                $('#createForm .loading').addClass('d-none') 
+                $('#modal').iziModal('close')
+                // reset form
+                $('#createForm .form-control').each((i,elem) => {
+                    $(elem).val('') 
+                })
+                popUpMessage({
+                    model : Swal,
+                    title : "Berhasil",
+                    text : "Data sopir berhasil ditambahkan"
+                })
+                $(form).removeClass('was-validated')
+            } catch (err) {
+                popUpMessage({
+                    model : Swal,
+                    title : "Gagal",
+                    text : err.data.message,
+                    icon : 'error'
+                })
+            }
+        }
+    })
+
+    //delete
+    $('table').on('click', '.deleteBtn' , (e) => {
+        e.preventDefault()
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                cancelButton: 'btn btn-danger p-2 w-25',
+                confirmButton: 'btn btn-success p-2 ml-4 w-25'
+            },
+            buttonsStyling: false
+        })
+        
+        swalWithBootstrapButtons.fire({
+            title: 'Apakah and ingin menghapus data ini ?',
+            text: "Data tidak dapat dikembalikan",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'No',
+            reverseButtons: true
+        }).then(async (result) => {
+            if (result.value) {
+                // delete proccess
+                const elem = $(e.target)
+                const data = sopirTable.row($(elem).parent()).data()
+                try {
+                    const result = await deleteSopir(data.id)
+                    popUpMessage({
+                        model : swalWithBootstrapButtons,
+                        title : 'Deleted!',
+                        text : 'Data sopir berhasil dihapus.'
+                    })
+                    drawTable(sopirTable)
+                } catch (err) {
+                    popUpMessage({
+                        model : swalWithBootstrapButtons,
+                        title : 'Error!',
+                        text : err.data.message,
+                        icon : 'error'
+                    })
+                }
+            }
+        })
+    })
     
     // live editing content
-    $('#sopirTable').on('focus', '.editable' , (event) => {
+    $('#sopirTable').on('dblclick', '.editable' , (event) => {
         const elem = event.target
-        $(elem).data('prevValue',$(elem).text())
+        const value = $(elem).text()
+        $(elem).data('prevValue',value)
+        $(elem).text('')
+        $(elem).append(`<textarea class="form-control" name="text" rows="2" cols="10" wrap="soft">${value}</textarea>`)
+        $(elem).find('textarea').focus()
     })
 
-    $('#sopirTable').on('input', '.editable' , (event) => {
+    $('#sopirTable').on('taphold', '.editable' , (event) => {
         const elem = event.target
+        const value = $(elem).text()
+        $(elem).data('prevValue',value)
+        $(elem).text('')
+        $(elem).append(`<textarea class="form-control" name="text" rows="2" cols="10" wrap="soft">${value}</textarea>`)
+        $(elem).find('textarea').focus()
+    })
+    
+    $('#sopirTable').on('blur', '.editable' , async (event) => {
+        const textarea = event.target //textarea
+        const elem = $(textarea).parent()
+
+        const value = $(textarea).val()
         const prevValue = $(elem).data('prevValue')
-        const newValue = $(elem).text()
-        if (prevValue != newValue) $(elem).addClass('hasChange')
-    })
+        if (value === prevValue) {
+            $(textarea).remove()
+            return $(elem).text(prevValue)
+        }
 
-    $('#sopirTable').on('blur', '.hasChange' , async (event) => {
-        const elem = event.target
         const colName = $(elem).data('colName')
         const row = $(elem).parent()    
 
         const cellInfo = sopirTable.cell(elem)[0][0]
-    
         const rowData = sopirTable.row(row).data()
-        const value = $(elem).text()
-        const prevValue = $(elem).data('prevValue')
+        
 
         const raw = {}
         raw[colName] = value
@@ -32,9 +139,21 @@ $(document).ready( async () => {
         try {
             await updateSopir(rowData.id,raw)
             await drawTable(sopirTable)
-            // console.log('success')
+            popUpMessage({
+                title : 'Perubahan berhasil disimpan'
+            })
+            $(textarea).remove()
             addAfterEditAnimations(cellInfo,sopirTable,true)
         } catch (err) {
+            Swal.fire({
+                // position: 'top-end',
+                icon: 'error',
+                title: 'Data tidak valid',
+                text : err.data.message,
+                showConfirmButton: true,
+                timer: 3000
+            })
+            
             addAfterEditAnimations(cellInfo,sopirTable,false)
             $(elem).text(prevValue)
         }
@@ -46,14 +165,21 @@ $(document).ready( async () => {
 })
 
 const defineDataTable = () => {
-    const sopirTable = $('#sopirTable').DataTable({
+    const sopir = $('#sopirTable').DataTable({
         columns : [
             {'defaultContent'  : ""},
             {"data" : 'nama'},
             {"data" : 'notelp'},
             {"data" : 'alamat'},
             {"data" : 'kodeSopir'},
-            {"data" : 'keterangan'}
+            {"data" : 'keterangan'},
+            {
+                "defaultContent" : 
+                `
+                <button class="btn btn-danger deleteBtn"><img src="/images/trash.svg" alt="delete"></button>
+                <button class="btn btn-info viewBtn"><img src="/images/eye.svg" alt="delete"></button>
+                `
+            }
         ],
         columnDefs: [
             {
@@ -78,22 +204,21 @@ const defineDataTable = () => {
                         default:
                             break
                     }
-                    $(td).attr('contenteditable', 'true')
                     $(td).addClass('editable')
                 }
             }
         ]
     })
     // tambah nomer
-    sopirTable.on( 'order.dt search.dt', function () {
-        sopirTable.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+    sopir.on( 'order.dt search.dt', function () {
+        sopir.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
             cell.innerHTML = i+1;
         } );
     } ).draw()
-    return sopirTable
+    return sopir
 }
 
-const drawTable = async (table = {}) => {
+const drawTable = async (table) => {
     const driverJson = await loadSopir()
     table.clear()
     table.rows.add(driverJson.data)
@@ -114,4 +239,48 @@ const addAfterEditAnimations = async ({row,column} , sopirTable, type) => {
             $(cell).removeClass('errEdit')
         }, 1000)
     }
+}
+
+const createValidation = () => {
+    let forms = $('.needs-validation')
+    // Loop over them and prevent submission
+    Array.prototype.filter.call(forms, (form) => {
+        $('.form-control').each((i,elem) => {
+            $(elem).next().text("")
+            $(elem).next().text(elem.validationMessage)
+        })
+        $(form).on('input', '.form-control' , (e) => {
+            const elem = e.target
+            if (elem.checkValidity() == false) {
+                $(elem).next().text("")
+                $(elem).next().text(elem.validationMessage)
+            }
+        })
+        if (form.checkValidity() === false) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+    })
+}
+
+const popUpMessage = ({
+    model = Swal,
+    title = "",
+    text = "",
+    showConfirmButton = false,
+    position = 'top-end',
+    backdrop = false,
+    timer = 2000,
+    icon = 'success'
+}) => {
+    model.fire({
+        position,
+        backdrop,
+        icon,
+        title,
+        text,
+        showConfirmButton,
+        timer
+    })
 }
--- public/js/systemHandler/sopirSystemHandler.js
+++ public/js/systemHandler/sopirSystemHandler.js
@@ -30,3 +30,34 @@ const updateSopir = async (id,data) => {
         }
     })
 }
+
+const createSopir = async (data) => {
+    return new Promise (async (resolve,reject) => {
+        try {
+            const result = await axios({
+                url : `${baseUrl}/api/driver/`,
+                method : 'POST',
+                data : data
+            })
+            resolve(result.data)
+        } catch (err) {
+            console.log(err.response)
+            reject(err.response)
+        }
+    })
+}
+
+const deleteSopir = async (id) => {
+    return new Promise (async (resolve,reject) => {
+        try {
+            const result = await axios({
+                url : `${baseUrl}/api/driver/${id}`,
+                method : 'DELETE'
+            })
+            resolve(result.data)
+        } catch (err) {
+            console.log(err.response)
+            reject(err.response)
+        }
+    })
+}
