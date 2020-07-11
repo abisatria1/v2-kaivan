29ad891ccd202f2469c4510075ceef52006d61d4
Abisatria1 <abi.kadek@gmail.com>
2020-07-11 18:23:33 +0800

    mengubah relasi database dan loggin database

--- config/relation.js
+++ config/relation.js
@@ -1,16 +1,22 @@
 const Customer = require('../models/Customer')
-const Order = require('../models/Order')
 const Driver = require('../models/Driver')
 const Partner = require('../models/Partner')
 const Payment = require('../models/Payment')
 const Secret = require('../models/Secret')
 const GoogleToken = require('../models/GoogleToken')
+const Order = require('../models/Order')
+const Contact = require('../models/Contact')
+
+
+Customer.belongsTo(Contact)
+Driver.belongsTo(Contact)
+Partner.belongsTo(Contact)
 
 Customer.hasMany(Order)
 Order.belongsTo(Customer)
 
 Driver.belongsToMany(Order, {through : 'order_driver'})
-Order.belongsToMany(Driver, {through : 'order_driver'})
+Order.belongsToMany(Driver, {through : 'order_driver' })
 
 Partner.hasMany(Order)
 Order.belongsTo(Partner)
