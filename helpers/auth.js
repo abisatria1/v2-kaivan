85f556f5a0e46e1062a65ea376b698374efc5d18
Abisatria1 <abi.kadek@gmail.com>
2020-07-11 23:55:40 +0800

    memperbaiki oauth callback ke env

--- helpers/auth.js
+++ helpers/auth.js
@@ -8,7 +8,7 @@ const Secret = require('../models/Secret')
 const googlePassport = new GoogleStrategy({
     clientID : process.env.GOOGLE_CLIENT_ID,
     clientSecret : process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL : 'http://localhost:3001/api/customer/google',
+    callbackURL : process.env.GOOGLE_CALLBACK_URL,
 },async (accessToken,refreshToken,profile,done) => {
     const all = await Secret.findAll({})
     for (let i = 0; i < all.length; i++) {
